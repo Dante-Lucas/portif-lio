62635c10cb9f9ad0e933b817cd95447ad725fb38
Dante-Lucas <157182980+Dante-Lucas@users.noreply.github.com>
2024-05-10 17:06:23 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 var list = []
-function envio(list){
+
+document.getElementById('enviar').addEventListener('click', function() {
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
     var mensagem = document.getElementById('mensagem').value;
-    list.unshift({'name':name,'email':email,'mensagem':mensagem});
+    list.unshift({'name': name, 'email': email, 'mensagem': mensagem});
     alert('Enviado com sucesso!!');
-    saveJSON(list);
-}
-function saveJSON(list){
-    json = JSON.stringify(list);
+    saveJSON();
+});
+
+function saveJSON() {
+    var json = JSON.stringify(list);
     console.log(json);
 }
